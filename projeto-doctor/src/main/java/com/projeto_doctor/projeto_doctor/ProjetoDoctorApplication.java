0a4b4fd8cbc80195abe33a847121ae66c0115c1e
Natan Silva <104787222+INatanSilva@users.noreply.github.com>
2024-11-29 23:10:05 +0000

    projeto-doctor

---
+++ projeto-doctor/src/main/java/com/projeto_doctor/projeto_doctor/ProjetoDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_doctor.projeto_doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDoctorApplication.class, args);
+	}
+
+}
---
+++ projeto-doctor/src/test/java/com/projeto_doctor/projeto_doctor/ProjetoDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_doctor.projeto_doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
